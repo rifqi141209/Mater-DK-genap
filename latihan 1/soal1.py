25f77ca446eed5a8000553871799e1b240e30650
rifqi141209 <muhamadrifqi234@gmail.com>
2025-01-14 07:47:05 +0700

    menambahkan latihan 1

---
+++ latihan 1/soal1.py
@@ -0,0 +1,6 @@
+#program menentukan apakah bilangan merupakan kelipatan 4
+bilangan = int(input("masukan bilangan : "))
+if bilangan % 4 == 0:
+    print ("bilangan merupakan kelipatan 4")
+else :
+    print ("bilangan bukan kelipatan 4")
