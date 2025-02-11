6e56078131d3abe051d72e2dfc6e47e7bb716c7f
rifqi141209 <muhamadrifqis234@gmail.com>
2025-02-11 07:43:32 +0700

    menambahkan folder 5

---
+++ latihan 5/r.py
@@ -0,0 +1,7 @@
+def identitassiswa(nama,kelas,jenis_kelamin):
+    print("nama : ",nama)
+    print("kelas ; 1A",kelas)
+    print("jenis kelamin:",jenis_kelamin)
+    
+#pangil fungsi cetak identitas siswa
+identitassiswa("ahmad","x-ds","ngerr")
